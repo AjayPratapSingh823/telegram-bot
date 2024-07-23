a6fab8baecc5bbb259141e2c0e437705aa70eaa4
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2024-07-23 16:49:57 +0530

    Update User.js

--- weather-bot-server/models/User.js
+++ weather-bot-server/models/User.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
     lname: { type: String,  },
     username: { type: String,unique:true  },
     telegramId: { type: Number }, // Optional, for storing Telegram ID
-    subscribed: { type: Boolean, default: false } // Optional, for subscription status
+    subscribed: { type: Boolean, default: false }, // Optional, for subscription status
+    latitude: Number, // Add latitude field
+    longitude: Number // Add longitude field
 });
 module.exports = mongoose.model('User', UserSchema);
