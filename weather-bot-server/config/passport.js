043b0836d2162582de9f05d3b7b93b927b647c86
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2024-07-23 15:14:55 +0530

    Update passport.js

--- weather-bot-server/config/passport.js
+++ weather-bot-server/config/passport.js
@@ -7,7 +7,7 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://telegram-bot-cp01.onrender.com/auth/google/callback'
+        callbackURL: 'https://telegram-bot-ygd7.onrender.com/auth/google/callback'
     }, async (accessToken, refreshToken, profile, done) => {
         const newUser = {
             googleId: profile.id,
